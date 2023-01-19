39e8f44ec96c0b7a7adef11ca5a8deeb5e00dc6e
hapihapi777 <rock666gemini69@gmail.com>
2023-01-19 22:40:05 +0000

    Implement advanced login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,6 +15,11 @@ class ActiveSupport::TestCase
     !session[:user_id].nil?
   end
 
+  # テストユーザーとしてログインする
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+
   # Run tests in parallel with specified workers
   # parallelize(workers: :number_of_processors)
 
@@ -22,4 +27,14 @@ class ActiveSupport::TestCase
   # fixtures :all
 
   # Add more helper methods to be used by all tests here...
+end
+
+class ActionDispatch::IntegrationTest
+
+  # テストユーザーとしてログインする
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
+  end
 end
