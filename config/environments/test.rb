329b02dfa5740eb025ea958c77c54f16d5c807ef
hapihapi777 <rock666gemini69@gmail.com>
2023-01-30 23:17:56 +0000

    リスト11.21でtoy_app/の方のファイルを編集していたみたいで戻ってきた。リスト11.22まで完了

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
-  config.action_mailer.default_url_options = { host: 'example.com' }
+  config.action_mailer.default_url_options = { host: 'example.com' } #リスト11.21(/sample_app/config/environments/test.rb)
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
