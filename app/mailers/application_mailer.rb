6c17048635be9f732e4febc8c2edf4d7b531a7b1
hapihapi777 <rock666gemini69@gmail.com>
2023-02-14 02:52:35 +0000

    13.7まで戻した

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "rock666gemini69@gmail.com"
+  default from: "rock666gemini69@gmail.com" #11.11にて設定を変更 12.12から戻ってきた。
   layout "mailer"
 end
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -9,6 +9,7 @@ class UserMailerTest < ActionMailer::TestCase
     assert_equal "Account activation", mail.subject
     assert_equal [user.email], mail.to
     assert_equal ["rock666gemini69@gmail.com"], mail.from # リスト12.20にて、redになる為12.12に戻ってきたら、こっち変えてなかった。
+    # 追記:11.11にて"/sample_app/app/mailers/application_mailer.rb"内のメールアドレスを編集しておく必要があった。
     # assert_equal ["user@realdomain.com"], mail.from
     assert_match user.name,               mail.body.encoded
     assert_match user.activation_token,   mail.body.encoded
@@ -22,6 +23,7 @@ class UserMailerTest < ActionMailer::TestCase
     assert_equal "Password reset", mail.subject
     assert_equal [user.email], mail.to
     assert_equal ["rock666gemini69@gmail.com"], mail.from # リスト12.12で["user@realdomain.com"]を["rock666gemini69@gmail.com"]に書き換えた。
+    # 追記:11.11にて"/sample_app/app/mailers/application_mailer.rb"内のメールアドレスを編集しておく必要があった。
     # assert_equal ["user@realdomain.com"], mail.from # リスト12.20にて、redになる為上の内容を元に戻した
     assert_match user.reset_token,        mail.body.encoded
     assert_match CGI.escape(user.email),  mail.body.encoded
